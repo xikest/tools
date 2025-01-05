0792110109af61af99531e9b7ef8ae6a9bb276b4
xik <xikest12@gmail..com>
2025-01-05 13:50:33 +0900

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xik_tools',
-    version='0.68',
+    version='0.69',
     author='xikest',
     description='tools for web',
     packages=find_packages(),
