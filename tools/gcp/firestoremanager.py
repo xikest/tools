e4b9205148207a11e71de517df183f36ba073536
xik <xikest12@gmail..com>
2025-01-28 12:44:43 +0900

    .

--- tools/gcp/firestoremanager.py
+++ tools/gcp/firestoremanager.py
@@ -27,48 +27,26 @@ class FirestoreManager:
         logging.info(f"Data saved successfully under collection {collection_name if collection_name else 'default_collection'}.")
 
 
-    def read_data(self, collection_name):
+    def read_data(self, collection_name: str) -> pd.DataFrame:
         logging.info(f"Reading data from collection: {collection_name}.")
         docs = self.db.collection(collection_name).stream()
         data = []
+        doc_ids = []  # 문서 키를 저장할 리스트
         
         for doc in docs:
             data.append(doc.to_dict())
+            doc_ids.append(doc.id)  # 문서의 키 값을 doc_ids에 추가
             logging.debug(f"Document {doc.id} retrieved successfully from {collection_name}.")
         
         if data:
-            df = pd.DataFrame(data)
+            df = pd.DataFrame(data, index=doc_ids)  # DataFrame의 인덱스로 doc_ids 사용
             logging.debug(f"Successfully converted {len(data)} documents to DataFrame.")
             return df
         else:
             logging.warning(f"No documents found in collection: {collection_name}.")
-            return pd.DataFrame() 
+            return pd.DataFrame()
 
-    
-    def save_list(self, dict_list: dict, collection_name="sample_collection"):
-        """
-        dict_list = {doc_key: data_list}
-        """
-        for doc_key, data_list in dict_list.items():
-            logging.info(f"Saving document with key: {doc_key}")  
-            doc_ref = self.db.collection(collection_name).document(doc_key)
-            logging.info(f"Document reference path: {doc_ref.path}")  
-            try:
-                doc_ref.set({'data_list': data_list})
-            except Exception as e:
-                logging.error(f"Error saving document: {e}")  
 
-    def read_list(self, doc_key, collection_name="sample_collection") -> list:
-        """
-        Firestore에서 리스트 데이터를 읽어옵니다.
-        """
-        doc_ref = self.db.collection(collection_name).document(doc_key)
-        doc = doc_ref.get()
-        
-        if doc.exists:
-            return doc.to_dict().get('data_list')
-        else:
-            return []
 
 
     def save_db(self, doc_key: str, data_dict: dict = None, collection_name: str = "sample_collection"):
@@ -93,6 +71,26 @@ class FirestoreManager:
         except Exception as e:
             logging.error(f"Error saving document: {e}")
             
+            
+    def get_all_document_keys(self, collection_name: str = "sample_collection")->list:
+        """
+        Firestore에서 특정 컬렉션의 모든 문서 키를 추출하는 함수.
+
+        Args:
+            collection_name (str): 컬렉션 이름 (기본값: "sample_collection").
+        
+        Returns:
+            list: 문서 키들의 리스트.
+        """
+        logging.info(f"Fetching all document keys from collection '{collection_name}'")
+        try:
+            docs = self.db.collection(collection_name).stream()
+            doc_keys = [doc.id for doc in docs]  # 문서의 id (키)를 추출
+            logging.info(f"Retrieved {len(doc_keys)} document keys successfully.")
+            return doc_keys
+        except Exception as e:
+            logging.error(f"Error fetching document keys: {e}")
+            return []   
             
     def is_doc_key_exist(self, doc_key: str, collection_name: str = "sample_collection") -> bool:
         """
@@ -120,3 +118,20 @@ class FirestoreManager:
             logging.error(f"Error checking document existence: {e}")
             return False
         
+        
+    def delete_document(self, doc_key: str, collection_name: str = "sample_collection"):
+        """
+        Firestore에서 특정 문서를 삭제하는 함수.
+
+        Args:
+            doc_key (str): 삭제할 문서의 고유 키.
+            collection_name (str): 컬렉션 이름 (기본값: "sample_collection").
+        """
+        logging.info(f"Attempting to delete document with key '{doc_key}' from collection '{collection_name}'")
+        doc_ref = self.db.collection(collection_name).document(doc_key)
+
+        try:
+            doc_ref.delete()
+            logging.info(f"Document with key '{doc_key}' deleted successfully.")
+        except Exception as e:
+            logging.error(f"Error deleting document: {e}")
